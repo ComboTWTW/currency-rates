d73f7ff9f12c3f1b780e57610f10fe0c29ebc430
ComboTWTW <gfakemail18@gmail.com>
2023-02-20 18:19:41 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Game from "./components/Game"
+
+const App = () => {
+  return (
+    <div>
+      <div className="flex flex-col items-center">
+        <Game />
+
+      </div>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Game.tsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react'
+
+const Game = () => {
+
+    const options = {
+        method: 'GET',
+        redirect: 'follow',
+        headers: ["apikey", "MFanPAxcMo4X34mhAR3V2iGdXgEi0Q7P"],
+    };
+
+    
+
+  return (
+    <div className='mt-10'>
+        <div className="text-black font-bold text-3xl">
+
+        </div>
+    </div>
+  )
+}
+
+export default Game
