261c29274774e1888df50156581d595fcfd44167
ComboTWTW <gfakemail18@gmail.com>
2023-02-28 02:01:11 +0500

    navbar almost done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,21 @@
+import logo from '../assets/logo.svg'
+import { links } from '../constants/index'
 
 const Navbar = () => {
   return (
     <div className=" bg-navbarBG flex flex-row justify-center">
-        <div className="leading-none container">test</div>
+        <div className="container flex flex-row justify-between items-center py-3 mx-3">
+          <div className="flex flex-row gap-4 items-center">
+            <a href="#"><img src={logo} alt="logo" className='w-[40px] h-[40px]'/></a>
+            <h2 className='font-semibold text-xl text-white'>CurrencyRates</h2>
+          </div>
+
+          <ul className='md:flex md:flex-row md:gap-16 hidden'>
+            {links.map((link) => (
+              <li className='text-white py-1 px-4 text-lg rounded-[10px] hover:bg-white hover:bg-opacity-30 hover:py-1 hover:px-4 hover:cursor-pointer duration-200'>{link.tittle}</li>
+            ))}
+          </ul>
+        </div>
     </div>
   )
 }
---
+++ src/constants/index.ts
@@ -0,0 +1,14 @@
+export const links = [
+    {
+        tittle: "Convert",
+        id: "Convert"    
+    },
+    {
+        tittle: "Chart",
+        id: "Chart"    
+    },
+    {
+        tittle: "Date",
+        id: "Date"    
+    },
+]
