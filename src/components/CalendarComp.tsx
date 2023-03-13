00b8685ab14d699730dc5edd7206ccfc93b7bc50
ComboTWTW <gfakemail18@gmail.com>
2023-03-13 20:27:52 +0500

    calendar doesn't work

--- src/components/CalendarComp.tsx
+++ src/components/CalendarComp.tsx
@@ -4,31 +4,39 @@ import * as React from 'react';
 import dayjs from 'dayjs';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
-import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
-import { border } from "@mui/system";
-
-
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 
 const CalendarComp = () => {
 
-    
-
+  
   return (
-    <div className="flex flex-col relative gap-2 md:min-w-0 md:flex-1">
+    <div className=" flex flex-col relative gap-2 md:min-w-0 md:flex-1">
         <h2 className={`${styles.cardBold}`}>Date</h2>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DesktopDatePicker format="DD/MM/YYYY" sx={{
-              '.MuiInputBase-input': {padding: "12px",},
+        <LocalizationProvider  dateAdapter={AdapterDayjs}>
+          
+            <DatePicker   format="DD/MM/YYYY" sx={{
+              '.MuiInputBase-input': {padding: "13px",
               borderRadius: '10px',
-              border: '#ddd 1px solid',
               outline: 'none',
+            },
+              borderRadius: '10px',
+              outline: 'none',
+              '.MuiOutlinedInput-root': {
+                borderRadius: '10px',
+                borderColor: '#ddd',
+                '&:hover': {
+                  outline: 'none',
+                },
+                '&:focus': {
+                  outline: 'none',
+                },
+              }
               
             }} className="shadow-md" defaultValue={dayjs()} />
-            <MobileDatePicker sx={{
-              display: 'none',
-            }} defaultValue={dayjs()} />
+          
+         
+
         </LocalizationProvider>
         
     </div>
